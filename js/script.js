c53d70daa7ab36ca82501e70c097f56043ee8c90
Clark Winkelmann <clark.winkelmann@gmail.com>
2015-01-30 15:57:14 +0100

    Added a shareable url for the score

--- js/script.js
+++ js/script.js
@@ -12,117 +12,320 @@
  */
 
 $(document).ready(function() {
-	function calculatePoints() {
+
+	/**
+	 * Missions properties
+	 * A 'bool' type is boolean that defaults to false
+	 * A 'option' type is a string that defaults to 'default'
+	 * A 'number' type is an integer that defaults to 0
+	 * @type {{name: string, type: string}[]}
+	 */
+	var missionsDescription = [
+		{ name: 'openingdoors',          type: 'bool'},
+		{ name: 'cloudaccess',           type: 'bool'},
+		{ name: 'communitylearning',     type: 'bool'},
+		{ name: 'roboticscompetition',   type: 'option'},
+		{ name: 'rightsenses',           type: 'bool'},
+		{ name: 'thinkingoutsidethebox', type: 'option'},
+		{ name: 'remotecommunication',   type: 'bool'},
+		{ name: 'searchengine',          type: 'option'},
+		{ name: 'sports',                type: 'option'},
+		{ name: 'reverseengineering',    type: 'option'},
+		{ name: 'changingconditions',    type: 'bool'},
+		{ name: 'apprenticeship',        type: 'option'},
+		{ name: 'projectlearning',       type: 'number'},
+		{ name: 'penaltypoints',         type: 'number'},
+		{ name: 'engagement-lever',      type: 'bool'},
+		{ name: 'engagement-rotations',  type: 'number'}
+	];
+
+	/**
+	 * @param missions Object of missions status
+	 * @returns {string} Url hash part
+	 */
+	function createShareUrl(missions) {
+		var i, missionDescription, value;
+
+		var urlParts = [];
+
+		for(i in missionsDescription) {
+			missionDescription = missionsDescription[i];
+
+			if(missions[missionDescription.name] != null) {
+				value = missions[missionDescription.name];
+
+				if(missionDescription.type == 'bool' && value != false
+					|| missionDescription.type == 'option' && value != 'default'
+					|| missionDescription.type == 'number' && value != 0) {
+					urlParts.push(missionDescription.name + (missionDescription.type == 'bool' ? '' : ':' + value));
+				}
+			}
+		}
+
+		return urlParts.join('|');
+	}
+
+	/**
+	 * @param url Url hash part
+	 * @returns {Object} Object of missions status
+	 */
+	function parseShareUrl(url) {
+		var i, missionDescription, value, j, urlPart, splittedPart;
+
+		var urlParts = url.split('|');
+
+		var missions = {};
+
+		for(i in missionsDescription) {
+			missionDescription = missionsDescription[i];
+
+			value = null;
+
+			for(j in urlParts) {
+				urlPart = urlParts[j];
+
+				// If the key is present in the url
+				if(urlPart.substr(0, missionDescription.name.length) == missionDescription.name) {
+					if(missionDescription.type == 'bool') {
+						value = true; // Present means true
+					} else {
+						splittedPart = urlPart.split(':');
+
+						switch(missionDescription.type) {
+							case 'option':
+								value = splittedPart[1];
+								break;
+							case 'number':
+								value = parseInt(splittedPart[1]);
+								break;
+						}
+					}
+
+					break;
+				}
+			}
+
+			// If the key was not found in the url
+			if(value == null) {
+				switch(missionDescription.type) {
+					case 'bool':
+						value = false;
+						break;
+					case 'option':
+						value = 'default';
+						break;
+					case 'number':
+						value = 0;
+						break;
+				}
+			}
+
+			missions[missionDescription.name] = value;
+		}
+
+		return missions;
+	}
+
+	/**
+	 * Fetches the current status of the ScoreBoard
+	 * @returns {Object} Missions status
+	 */
+	function getCurrentMissionsStatus() {
+		var i, missionDescription, value;
+
+		var missions = {};
+
+		for(i in missionsDescription) {
+			missionDescription = missionsDescription[i];
+
+			value = '';
+
+			if(missionDescription.type == 'bool') {
+				value = $('input[name=fll-' + missionDescription.name + ']').is(':checked');
+			} else if(missionDescription.type == 'option') {
+				value = $('input[name=fll-' + missionDescription.name + ']:checked').val();
+			} else if(missionDescription.type == 'number') {
+				value = parseInt($('input[name=fll-' + missionDescription.name + ']').val());
+			}
+
+			missions[missionDescription.name] = value;
+		}
+
+		return missions;
+	}
+
+	/**
+	 * Apply the given status to the ScoreBoard
+	 * @param missions Missions status
+	 */
+	function setCurrentMissionStatus(missions) {
+		var i, missionDescription, value;
+
+		for(i in missionsDescription) {
+			missionDescription = missionsDescription[i];
+
+			value = missions[missionDescription.name];
+
+			if(missionDescription.type == 'bool') {
+				value = $('input[name=fll-' + missionDescription.name + ']').prop('checked', value);
+			} else if(missionDescription.type == 'option') {
+				value = $('input[name=fll-' + missionDescription.name + '][value=' + value + ']').prop('checked', true);
+			} else if(missionDescription.type == 'number') {
+				value = $('input[name=fll-' + missionDescription.name + ']').val(value);
+			}
+
+			missions[missionDescription.name] = value;
+		}
+	}
+
+	/**
+	 * Updates the UI according to current status
+	 * @param missions Missions status
+	 */
+	function calculatePoints(missions) {
 		var points = 0;
-		
+
 		// Opening Doors
-		var score_openingdoors = $('input[name=fll-openingdoors]').is(':checked') ? 15 : 0;
+		var score_openingdoors = missions.openingdoors ? 15 : 0;
 		points += score_openingdoors;
 		$('.mission.openingdoors .score').html(score_openingdoors);
-		
+
 		// Cloud Access
-		var score_cloudaccess = $('input[name=fll-cloudaccess]').is(':checked') ? 30 : 0;
+		var score_cloudaccess = missions.cloudaccess ? 30 : 0;
 		points += score_cloudaccess;
 		$('.mission.cloudaccess .score').html(score_cloudaccess);
-		
+
 		// Community Learning
-		var score_communitylearning = $('input[name=fll-communitylearning]').is(':checked') ? 25 : 0;
+		var score_communitylearning = missions.communitylearning ? 25 : 0;
 		points += score_communitylearning;
 		$('.mission.communitylearning .score').html(score_communitylearning);
-		
+
 		// Robotics Competition
-		var roboticscompetition_val = $('input[name=fll-roboticscompetition]:checked').val();
-		var score_roboticscompetition = roboticscompetition_val == 'released' ? 55 : roboticscompetition_val == 'insert' ? 25 : 0;
+		var score_roboticscompetition = missions.roboticscompetition == 'released' ? 55 : missions.roboticscompetition == 'insert' ? 25 : 0;
 		points += score_roboticscompetition;
 		$('.mission.roboticscompetition .score').html(score_roboticscompetition);
-		
+
 		// Using the Right Senses
-		var score_rightsenses = $('input[name=fll-rightsenses]').is(':checked') ? 40 : 0;
+		var score_rightsenses = missions.rightsenses ? 40 : 0;
 		points += score_rightsenses;
 		$('.mission.rightsenses .score').html(score_rightsenses);
-		
+
 		// Thinking Outside the Box
-		var thinkingoutsidebox_val = $('input[name=fll-thinkingoutsidebox]:checked').val();
-		var score_thinkingoutsidebox = thinkingoutsidebox_val == 'faceup' ? 40 : thinkingoutsidebox_val == 'facedown' ? 25 : 0;
-		points += score_thinkingoutsidebox;
-		$('.mission.thinkingoutsidebox .score').html(score_thinkingoutsidebox);
-		
+		var score_thinkingoutsidethebox = missions.thinkingoutsidethebox == 'up' ? 40 : missions.thinkingoutsidethebox == 'down' ? 25 : 0;
+		points += score_thinkingoutsidethebox;
+		$('.mission.thinkingoutsidethebox .score').html(score_thinkingoutsidethebox);
+
 		// Remote Communication / Learning
-		var score_remotecommunication = $('input[name=fll-remotecommunication]').is(':checked') ? 40 : 0;
+		var score_remotecommunication = missions.remotecommunication ? 40 : 0;
 		points += score_remotecommunication;
 		$('.mission.remotecommunication .score').html(score_remotecommunication);
-		
+
 		// Search Engine
-		var searchengine_val = $('input[name=fll-searchengine]:checked').val();
-		var score_searchengine = searchengine_val == 'removed' ? 60 : searchengine_val == 'pushed' ? 15 : 0;
+		var score_searchengine = missions.searchengine == 'removed' ? 60 : missions.searchengine == 'pushed' ? 15 : 0;
 		points += score_searchengine;
 		$('.mission.searchengine .score').html(score_searchengine);
-		
+
 		// Sports
-		var sports_val = $('input[name=fll-sports]:checked').val();
-		var score_sports = sports_val == 'goal' ? 60 : sports_val == 'shoot' ? 30 : 0;
+		var score_sports = missions.sports == 'goal' ? 60 : missions.sports == 'shoot' ? 30 : 0;
 		points += score_sports;
 		$('.mission.sports .score').html(score_sports);
-		
+
 		// Reverse Engineering
-		var reverseengineering_val = $('input[name=fll-reverseengineering]:checked').val();
-		var score_reverseengineering = reverseengineering_val == 'replicated' ? 45 : reverseengineering_val == 'base' ? 30 : 0;
+		var score_reverseengineering = missions.reverseengineering == 'replicated' ? 45 : missions.reverseengineering == 'base' ? 30 : 0;
 		points += score_reverseengineering;
 		$('.mission.reverseengineering .score').html(score_reverseengineering);
-		
+
 		// Adapting to Changing Conditions
-		var score_changingconditions = $('input[name=fll-changingconditions]').is(':checked') ? 15 : 0;
+		var score_changingconditions = missions.changingconditions ? 15 : 0;
 		points += score_changingconditions;
 		$('.mission.changingconditions .score').html(score_changingconditions);
-		
+
 		// Apprenticeship
-		var apprenticeship_val = $('input[name=fll-apprenticeship]:checked').val();
-		var score_apprenticeship = apprenticeship_val == 'circle' ? 35 : apprenticeship_val == 'built' ? 20 : 0;
+		var score_apprenticeship = missions.apprenticeship == 'placed' ? 35 : missions.apprenticeship == 'built' ? 20 : 0;
 		points += score_apprenticeship;
 		$('.mission.apprenticeship .score').html(score_apprenticeship);
-		
+
 		// Project Based Learning
-		var projetlearning_loops = $('input[name=fll-projetlearning-loops]').val();
-		var projetlearning_additionalloops = projetlearning_loops > 0 ? projetlearning_loops-1 : 0;
-		var score_projetlearning = (projetlearning_loops > 0 ? 20 : 0) + // First loop
-			projetlearning_additionalloops*10; // Other loops
-		points += score_projetlearning;
-		$('.mission.projetlearning .score').html(score_projetlearning);
-		
+		var projectlearning_loops = missions['projectlearning'];
+		var projectlearning_additionalloops = projectlearning_loops > 0 ? projectlearning_loops-1 : 0;
+		var score_projectlearning = (projectlearning_loops > 0 ? 20 : 0) + // First loop
+			projectlearning_additionalloops*10; // Other loops
+		points += score_projectlearning;
+		$('.mission.projectlearning .score').html(score_projectlearning);
+
 		// Penalty Points
-		var score_penaltypoints = $('input[name=fll-penaltypoints]').val()*-10;
+		var score_penaltypoints = missions.penaltypoints*-10;
 		points += score_penaltypoints;
 		$('.mission.penaltypoints .score').html(score_penaltypoints);
-		
+
 		// Engagement
-		var engagement_lever = $('input[name=fll-engagement-lever]').is(':checked');
-		var engagement_rotations = parseInt($('input[name=fll-engagement-rotations]').val());
+		var engagement_lever = missions['engagement-lever'];
+		var engagement_rotations = missions['engagement-rotations'];
 		var additional_percent = engagement_lever ? engagement_rotations > 0 ? engagement_rotations+9 : 0 : 0;
-		
+
 		$('#totaldetails').html(engagement_lever && engagement_rotations > 0 ? '( '+points+' subtotal + '+additional_percent+'% + 20 )' : '');
 		var score_engagementwheel = (additional_percent/100)*points;
 		points += score_engagementwheel;
-		var score_engagementlever = engagement_lever ? 20 : 0
+		var score_engagementlever = engagement_lever ? 20 : 0;
 		points += score_engagementlever; // Lever points added in last place
 		$('.mission.engagement .score').html(score_engagementlever+' + '+score_engagementwheel);
-		
+
 		$('.totalpoints').html(points);
 	}
-	
-	// On every change
-	$('input').change(calculatePoints);
-	
-	// We do not want to send the form
-	$('form').on('submit',function(e) {
+
+	/**
+	 * Handle a change in the form
+	 */
+	function handleChange() {
+		var missions = getCurrentMissionsStatus();
+
+		calculatePoints(missions);
+
+		var url = createShareUrl(missions);
+
+		if(url == '' && window.history && window.history.pushState) {
+			window.history.pushState('', '', window.location.pathname); // Remove the hash completely
+		} else {
+			location.hash = url;
+		}
+	}
+
+	/**
+	 * Handle the reset of the form
+	 */
+	function handleReset() {
+		$('form').get(0).reset();
+
+		handleChange();
+	}
+
+	/**
+	 * Handle the initialisation of the page
+	 */
+	function init() {
+		if(location.hash != '' && location.hash != '#') {
+			setCurrentMissionStatus(parseShareUrl(location.hash.replace('#', '')));
+		}
+
+		handleChange();
+	}
+
+	/**
+	 * Event bindings
+	 */
+	$('input').change(handleChange);
+	$('.reset').click(handleReset);
+
+	/**
+	 * We do not want the form to be sent
+	 */
+	$('form').on('submit', function(e) {
 		e.preventDefault();
 	});
-	
-	// Reset Button
-	$('.reset').click(function() {
-		$('form')[0].reset();
-		calculatePoints();
-	});
-	
-	// Init
-	calculatePoints();
+
+	/**
+	 * Turn on the lights !
+	 */
+	init();
+
 });
